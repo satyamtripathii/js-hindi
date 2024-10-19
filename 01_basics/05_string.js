b04e5b70b25ab374696b90fc15bafc041a1cf2a0
satyamtripathii <satyamtripathi978@gmail.com>
2024-10-19 09:45:07 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+// const name = "hitesh"
+// const repocount =30
+
+// console.log( name + repocount +" value")
+
+// console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+
+const gamename = new String('satyam')
+ console.log(gamename[0])
+ console.log(gamename.__proto__)
+ console.log(gamename.length)
+ console.log(gamename.toUpperCase())
+ console.log(gamename.charAt(4))
+ console.log(gamename.indexOf('y'))
