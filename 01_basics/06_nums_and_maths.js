da03b72242cb9da24543c21abf15dfcd4a851c2b
satyamtripathii <satyamtripathi978@gmail.com>
2024-10-19 09:46:10 +0000

    nums_and_maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,33 @@
+//  ****numbers*****
+
+// const score =400
+// console.log(score);
+
+// const balance = new Number(2000)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2))
+
+// const othernumber = 23.4567
+// console.log(othernumber.toPrecision(3))
+
+// const newnumber = 100000000;
+// console.log(newnumber.toLocaleString('en-IN'));
+
+/********* MATHS ********/
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.5))
+// console.log(Math.floor(4.9))
+// console.log(Math.ceil(4.1))
+// console.log(Math.min(2,4,5,6,-1))
+// console.log(Math.max(2,3,4,-1,5))
+
+console.log(Math.random())
+console.log(Math.random()*10 + 1)
+
+const min =10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
